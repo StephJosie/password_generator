26fb8af366bf85ca34d50fa412fd6fd54f08f442
steph <sjapodaca1103@gmail.com>
2021-10-02 22:31:21 -0700

    third push

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # password_generator
+## Description
+This project was built in order to help generate passwords. The passwords should 
+generate using uppercase, lowercase, and numbers.
+
+## Usage
+This generator is fairly simple to use, it automatically generates a password that contains 12 characters.  The characters are a combination of uppercase, lowercase, and numbers.
--- assets/script.js
+++ assets/script.js
@@ -2,45 +2,55 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-
-    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-var resultsA = [];
-var usedItemsArray = [];
-
-
-
-
-    var characters = prompt ("Please choose a number between 8 and 15");
-    var numPrompt = alert ("Password will contain numbers");
+    
+    var characters = alert ("You will be given a password of 12 characters");
+    var numPrompt = alert ("Password may contain numbers");
     var upper = alert ("Would you like to include Upper Case letters?");
     var lower = alert ("Would you like your password to contain lower case letters?");
 
+    var characters = 12,
+    charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    retVal = "";
+for (var i = 0, n = charset.length; i < characters; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+}
+return retVal;
+}
+
+//     var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//     var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//     var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// var resultsA = [];
+// var usedItemsArray = [];
 
 
-    if (numPrompt) {
-        resultsA = resultsA.concat(numbers);
-    };
 
 
-    if (upper) {
-        resultsA = resultsA.concat(upperCase);
-    };
+    
 
 
-    if (lower) {
-        resultsA = resultsA.concat(lowerCase)
-    };
+    // if (numPrompt) {
+    //     resultsA = resultsA.concat(numbers);
+    // };
 
-    console.log(resultsA)
 
-    for (var i = 0; i < characters; i++) {
-        usedItemsArray.push (resultsA[Math.floor(Math.random() * resultsA.length)])
-    }
+    // if (upper) {
+    //     resultsA = resultsA.concat(upperCase);
+    // };
 
-    return usedItemsArray.join("")
+
+    // if (lower) {
+    //     resultsA = resultsA.concat(lowerCase)
+    // };
+
+    // console.log(resultsA)
+
+    // for (var i = 0; i < characters; ++i) {
+    //     usedItemsArray.push.call (resultsA[Math.floor(Math.random() * resultsA.length)])
+    // }
+
+    // return usedItemsArray.join("")
 
 //    retVal = (""); 
 //     for (var i = 0; n = characters.length; i < length; ++i) {
@@ -53,11 +63,12 @@ var usedItemsArray = [];
     // }
     // return retVal;
     
-
+    // function generatePassword() {
+       
 
 
     // return usedItemsArray.join("");
-}
+
 
 
     
