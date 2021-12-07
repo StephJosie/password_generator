51e80a402e3d51b7832bf2255b9e6a1c1bb7916d
steph <sjapodaca1103@gmail.com>
2021-12-06 18:13:54 -0800

    deleted unnecessary comments

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,34 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-    
-    var characters = alert ("You will be given a password of 12 characters");
-    var numPrompt = alert ("Password may contain numbers");
-    var upper = alert ("Would you like to include Upper Case letters?");
-    var lower = alert ("Would you like your password to contain lower case letters?");
+
+    var characters = alert("You will be given a password of 12 characters");
+    var numPrompt = alert("Password may contain numbers");
+    var upper = alert("Would you like to include Upper Case letters?");
+    var lower = alert("Would you like your password to contain lower case letters?");
 
     var characters = 12,
-    charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    retVal = "";
-for (var i = 0, n = charset.length; i < characters; ++i) {
-    retVal += charset.charAt(Math.floor(Math.random() * n));
+        charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < characters; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
 }
-return retVal;
-}
-
-//     var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//     var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//     var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-// var resultsA = [];
-// var usedItemsArray = [];
 
 
 
 
-    
-
-
-    // if (numPrompt) {
-    //     resultsA = resultsA.concat(numbers);
-    // };
-
-
-    // if (upper) {
-    //     resultsA = resultsA.concat(upperCase);
-    // };
-
-
-    // if (lower) {
-    //     resultsA = resultsA.concat(lowerCase)
-    // };
-
-    // console.log(resultsA)
-
-    // for (var i = 0; i < characters; ++i) {
-    //     usedItemsArray.push.call (resultsA[Math.floor(Math.random() * resultsA.length)])
-    // }
-
-    // return usedItemsArray.join("")
-
-//    retVal = (""); 
-//     for (var i = 0; n = characters.length; i < length; ++i) {
-//         usedItemsArray.push (resultsA [Math.floor(Math.random() * n.)]);
-//      }
-
-    //  retVal = ("");
-    // for (var i = 0, n = characters.length; i < length; ++i) {
-    //   retVal += resultsA.charAt(Math.floor(Math.random() * characters.length));
-    // }
-    // return retVal;
-    
-    // function generatePassword() {
-       
-
-
-    // return usedItemsArray.join("");
 
 
 
-    
 
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 }
 
 
@@ -91,13 +40,5 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-    
 
-// function generatePassword() {
-//     var length = 14
-//     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    // retVal = "";
-    // for (var i = 0, n = charset.length; i < length; ++i) {
-    //   retVal += charset.charAt(Math.floor(Math.random() * n));
-    // }
-//     return retVal;
+
